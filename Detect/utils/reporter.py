f958e48c93b6f02c42e5c2f3f2096e3e9992b732
samkeppler <sck@kepplerworld.com>
2025-05-06 18:22:07 +0000

    fix indent

--- Detect/utils/reporter.py
+++ Detect/utils/reporter.py
@@ -266,8 +266,8 @@ def plot_features(x, x_hat, mae, p_along, p_overall, p_div, subject, metric, gro
     st.success("Mean Absolute Error (MAE, unscaled): " + str(np.round(np.mean(mae), 3)))
 
     if p_div == 0:
-    st.error("Division by zero: p_div is 0. Check if any tracts or subjects were selected.")
-    return None, None  # or return dummy values depending on your function
+        st.error("Division by zero: p_div is 0. Check if any tracts or subjects were selected.")
+        return None, None  # or return dummy values depending on your function
 
     if (p_overall < max(0.01, (1/p_div))):
         st.success("p < "+str(np.round(1/p_div,3)))
