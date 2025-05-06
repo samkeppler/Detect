aa5e2579f856a8fe12ae747507e1592f6ce5e929
samkeppler <sck@kepplerworld.com>
2025-05-06 21:32:54 +0000

    update tract list

--- Detect/inspect-demo.py
+++ Detect/inspect-demo.py
@@ -28,9 +28,10 @@ def main():
 
     subject = st.sidebar.selectbox("Subject", df_demog.ID)
 
-    tract_list = ['AF_left', 'AF_right','ATR_left','ATR_right', 'CA', 'CC_1', 'CC_2', 'CC_3', 'CC_4', 'CC_5', 'CC_6', 'CC_7', 'CG_left', 'CG_right', 
-                  'CST_left', 'CST_right', 'FX_left', 'FX_right', 'IFO_left', 'IFO_right', 'ILF_left', 'ILF_right', 'OR_left', 'OR_right', 'SLF_I_left',
-                  'SLF_II_left', 'SLF_III_left', 'SLF_I_right', 'SLF_II_right', 'SLF_III_right', 'UF_left', 'UF_right'] 
+    tract_list = ['AF_left', 'AF_right','ATR_left','ATR_right', 'CC_1', 'CC_2', 'CC_3', 'CC_4', 'CC_5', 'CC_6', 'CC_7', 'CG_left', 'CG_right', 
+                  'CST_left', 'CST_right', 'FPT_left', 'FPT_right', 'IFO_left', 'IFO_right', 'ILF_left', 'ILF_right', 'OR_left', 'OR_right', 
+                  'POPT_left', 'POPT_right', 'SLF_I_left','SLF_II_left', 'SLF_III_left', 'SLF_I_right', 'SLF_II_right', 'SLF_III_right', 
+                  'UF_left', 'UF_right'] 
     selected_tracts = st.sidebar.multiselect("Select Tracts", tract_list, default=tract_list)
 
     regress = st.sidebar.checkbox("Regress Confounds", value=True)
