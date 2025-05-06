a06c43b0c10c9f22c69fa1ba8e4ef10eafc5cabc
samkeppler <sck@kepplerworld.com>
2025-05-06 20:57:46 +0000

    rename model functions

--- Detect/models/zscore.py
+++ Detect/models/zscore.py
@@ -23,5 +23,15 @@ def run(model):
     #MAE_test = np.mean((X_test - meanProfile), axis =1)
     
     return zscoreTrain, zscoreTest
+
+class ZScoreModel:
+    def __init__(self):
+        pass
+
+    def run(self, X_train, X_test):
+        mean = np.mean(X_train, axis=0)
+        std = np.std(X_train, axis=0)
+        z_test = np.mean((X_test - mean) / std, axis=1)
+        return None, z_test
     
     
