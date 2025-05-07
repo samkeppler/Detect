f9f95d992185fa2560b717b7aa6fdf92c4abc12e
samkeppler <sck@kepplerworld.com>
2025-05-07 21:30:48 +0000

    restore reporter

--- Detect/utils/reporter.py
+++ Detect/utils/reporter.py
@@ -12,7 +12,7 @@ import seaborn as sns
 import base64
 from numpy import interp
 
-from models import zscore, pca, autoencoder
+from models import Zscore, PCA, autoencoder
 
 from matplotlib.backends.backend_agg import RendererAgg
 _lock = RendererAgg.lock
