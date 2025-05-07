b8daf89f93300f9050df5d827910880153909359
samkeppler <sck@kepplerworld.com>
2025-05-07 22:00:28 +0000

    add in def run_once

--- Detect/models/pca.py
+++ Detect/models/pca.py
@@ -108,3 +108,8 @@ class PCAModel:
         # For PCA + Mahalanobis, inverse transform isn't meaningful.
         # We'll return zeros so MAE = |score| works in inspector.
         return np.zeros_like(X_scores)
+    
+    def run_once(model_instance):
+        model = PCAModel()
+        model.fit(model_instance.X_train)
+        return model.transform(model_instance.X_test)
